2e6d75bcd8e855947b4d0a5a1122e2b5644e127e
DoroNyong <62960758+DoroNyong@users.noreply.github.com>
2023-08-23 12:11:15 +0900

    레포지토리 생성

---
+++ ConsoleAppHyeongyo_RPG/ConsoleAppHyeongyo_RPG/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppHyeongyo_RPG
+{
+    // 플레이어 클래스
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public int Level { get; set; }
+        public int Atk { get; set; }
+        public int Def { get; set; }
+        public int Hp { get; set; }
+        public int Gold { get; set; }
+        public int EquipAtk { get; set; } = 0;
+        public int EquipDef { get; set; } = 0;
+
+        // 플레이어 생성자
+        public Player(string name, string job, int level, int atk, int def, int hp, int gold)
+        {
+            Name = name;
+            Job = job;
+            Level = level;
+            Atk = atk;
+            Def = def;
+            Hp = hp;
+            Gold = gold;
+        }
+    }
+}
---
+++ ConsoleAppHyeongyo_RPG/ConsoleAppHyeongyo_RPG/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ConsoleAppHyeongyo_RPG
+{
+    internal class Program
+    {
+        // 플레이어 객체 생성
+        public static Player player;
+
+        // 아이템 객체 생성
+        public static Items.Weapon weapon01;
+        public static Items.Armor armor01;
+        public static Items.Weapon weapon02;
+        public static Items.Armor armor02;
+
+        // 아이템 리스트 생성
+        public static List<Items.Item> items= new List<Items.Item>();
+        
+        // 캐릭터, 아이템 정보 세팅
+        static void GameDataSetting()
+        {
+            // 캐릭터 정보 세팅
+            player = new Player("Player", "전사", 1, 10, 5, 20, 150);
+
+            // 아이템 정보 세팅
+            weapon01 = new Items.Weapon("낡은 검", "쉽게 볼 수 있는 낡은 검 입니다.", 2);
+            items.Add(weapon01);
+            armor01 = new Items.Armor("무쇠갑옷", "무쇠로 만들어져 튼튼한 갑옷입니다.", 5);
+            items.Add(armor01);
+            weapon02 = new Items.Weapon("나만의 대검", "나만의 대검 입니다.", 5);
+            items.Add(weapon02);
+            armor02 = new Items.Armor("천갑옷", "천으로 만들어져 가볍습니다.", 2);
+            items.Add(armor02);
+        }
+
+        // 선택지 객체 생성
+        public static Option option  = new Option();
+
+        static void Main(string[] args)
+        {
+            // 세팅 시작
+            GameDataSetting();
+            // 시작 선택지 실행
+            option.option(Option.Action.Start);
+        }
+    }
+}
